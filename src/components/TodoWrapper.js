e4479398719e469eae787f9aafb7908e5d86d787
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-04-25 14:26:10 +0900

    feat : todo wrapper

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { TodoForm } from './TodoForm'
 
 export const TodoWrapper = () => {
     return (
-        <div>TodoWrapper</div>
+        // tag 안에 className을 작성하게 된다면 App CSS에 존재하는 Block을 가져옵니다.
+        <div className='TodoWrapper'>
+            <TodoForm/>
+        </div>
     )
-        
 }
