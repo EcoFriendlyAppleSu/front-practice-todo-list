f3ffd273f346bad421bce2c4517ba11da5b1a596
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-04-25 15:29:00 +0900

    complete tutorial

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,13 +3,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const Todo = ({task, toggleComplete}) => {
+export const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
     return (
         <div className='Todo'>
             <p className={`${task.completed ? 'completed' : ""}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
             <div>
-                <FontAwesomeIcon icon={faPenToSquare}/>
-                <FontAwesomeIcon icon={faTrash}/>
+                <FontAwesomeIcon icon={faPenToSquare} onClick = {() => editTodo(task.id)}/>
+                {/* () => 를 사용해 내부 값을 바로 넘겨줄 수 있습니다. */}
+                <FontAwesomeIcon icon={faTrash} onClick = {() => deleteTodo(task.id)}/>
             </div>
         </div>
     )
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import { TodoForm } from './TodoForm'
 import {v4 as uuidv4} from 'uuid';
 import { Todo } from './Todo';
+import { EditTodoForm } from './EditTodoForm';
 uuidv4();
 
 export const TodoWrapper = () => {
@@ -30,16 +31,25 @@ export const TodoWrapper = () => {
             )
     }
 
+    const editTask = (task, id) => {
+        setTodos(todos.map(todo => todo.id === id ? {
+            ...todo, task, isEditing: !todo.isEditing} : todo))
+    }
+
     return (
         // tag 안에 className을 작성하게 된다면 App CSS에 존재하는 Block을 가져옵니다.
         <div className='TodoWrapper'>
             <h1>GET THINGS DONE!</h1>
             <TodoForm addTodo={addTodo}/>
             {todos.map((todo, index) => (
-                <Todo task={todo} key={index}
-                toggleComplete={toggleComplete}
-                deleteTodo = {deleteTodo}
-                editTodo = {editTodo}/>
+                todo.isEditing ? (
+                    <EditTodoForm editTodo={editTask} task={todo}/>
+                ) : (
+                    <Todo task={todo} key={index}
+                    toggleComplete={toggleComplete}
+                    deleteTodo = {deleteTodo}
+                    editTodo = {editTodo}/>
+                )
             ))}
         </div>
     )
